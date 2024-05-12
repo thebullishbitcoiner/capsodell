75d162624e30a3e3e80456ba06a928e8a6db3ab8
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2024-05-11 21:07:41 -0500

    v42.0.67

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'odellisms-cache-v42.0.66';
+const CACHE_NAME = 'odellisms-cache-v42.0.67';
 const urlsToCache = [
   '/index.html',
   '/ODELLISMS.TXT',
@@ -12,20 +12,20 @@ self.addEventListener('install', event => {
     );
 });  
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => {
-          return cacheName.startsWith('odellisms-cache') &&
-                 cacheName !== CACHE_NAME;
-        }).map(cacheName => {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
+// self.addEventListener('activate', event => {
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//         cacheNames.filter(cacheName => {
+//           return cacheName.startsWith('odellisms-cache') &&
+//                  cacheName !== CACHE_NAME;
+//         }).map(cacheName => {
+//           return caches.delete(cacheName);
+//         })
+//       );
+//     })
+//   );
+// });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
