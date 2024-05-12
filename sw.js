b783d9b9c625485a9f4af0670a31471807dba0bb
thebullishbitcoiner <139607837+thebullishbitcoiner@users.noreply.github.com>
2024-05-11 21:03:38 -0500

    v42.0.65

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = 'odellisms-cache-v42.0.64';
+const CACHE_NAME = 'odellisms-cache-v42.0.65';
 const urlsToCache = [
   '/index.html',
   '/ODELLISMS.TXT',
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
-});
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+        .then(() => self.skipWaiting()) // Activate the new service worker immediately
+    );
+});  
 
 self.addEventListener('activate', event => {
   event.waitUntil(
@@ -38,3 +38,28 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+        return Promise.all(
+            cacheNames.filter(cacheName => {
+            return cacheName.startsWith('odellisms-cache') &&
+                    cacheName !== CACHE_NAME;
+            }).map(cacheName => {
+            return caches.delete(cacheName);
+            })
+        );
+        })
+        .then(() => self.clients.claim()) // Activate the new service worker immediately
+        .then(() => {
+        // Send a message to the client page when the new version is activated
+        self.clients.matchAll().then(clients => {
+            clients.forEach(client => {
+            client.postMessage({ type: 'service-worker-update' });
+            });
+        });
+        })
+    );
+});
+  
